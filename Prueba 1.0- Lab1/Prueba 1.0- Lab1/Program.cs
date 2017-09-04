fbbce1550b0e88fdcbf377d43025aaf81b00905b
SC_Jackie <jackiesalazar20@gmail.com>
2017-09-04 17:22:31 -0600

    Create a new project to start working! :D

---
+++ Prueba 1.0- Lab1/Prueba 1.0- Lab1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba_1._0__Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
